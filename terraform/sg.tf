3d1f3fe54e84ceba371e37d4ad15b7152967ff93
amuhtarov <amuhtarov@akumoproject.com>
2023-07-06 13:49:49 -0500

    changes:

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -6,14 +6,14 @@
         from_port        = var.ports[2]
         to_port          = var.ports[2]
         protocol         = var.protocol[1]
-        cidr_blocks      = var.cidrs[1]
+        cidr_blocks      = [var.cidrs[1]]
     }
 
     egress {
         from_port        = var.ports[0]
         to_port          = var.ports[0]
         protocol         = var.protocol[0]
-        cidr_blocks      = var.cidrs[0]
+        cidr_blocks      = [var.cidrs[0]]
     }
     
 }
