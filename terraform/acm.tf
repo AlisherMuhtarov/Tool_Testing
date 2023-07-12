241bab09128d8942407551eaa710424099c03917
amuhtarov <amuhtarov@akumoproject.com>
2023-07-12 17:53:44 -0500

    changes

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -10,6 +10,23 @@ resource "aws_route53_record" "app_validation" {
   type    = "CNAME"
   ttl     = "300"
   records = [aws_acm_certificate_validation.app.validation_record_fqdn]
+
+  depends_on = [aws_acm_certificate_validation.app]
+}
+
+resource "null_resource" "app_dns_validation" {
+  depends_on = [aws_acm_certificate.app, aws_route53_zone.app_zone]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      aws route53 create-resource-record \
+        --hosted-zone-id "${aws_route53_zone.app_zone.zone_id}" \
+        --name "_acme-challenge.virmzi.de." \
+        --type "CNAME" \
+        --ttl 300 \
+        --resource-records '[{ "Value": "${aws_acm_certificate.app.domain_validation_options.0.resource_record_name}" }]'
+    EOT
+  }
 }
 
 resource "aws_acm_certificate_validation" "app" {
