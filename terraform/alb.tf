25c77bd43dbb0e746b2f3e8b16facbb55b05aa0f
amuhtarov <amuhtarov@akumoproject.com>
2023-07-12 16:07:19 -0500

    changes

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "test" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.main_sg.id]
-  subnets            = ["subnet-098ab2c379f487d8e"]
+  subnets            = ["subnet-098ab2c379f487d8e", "subnet-00c67e8302ea518f9", "subnet-01b15e0262af666eb"]
   
   ip_address_type = "ipv4"
   enable_deletion_protection = true
