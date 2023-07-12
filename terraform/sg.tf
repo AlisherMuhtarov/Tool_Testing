cde7517f3c62f9d0cbf69eafbece31e3a5fad373
amuhtarov <amuhtarov@akumoproject.com>
2023-07-12 15:37:37 -0500

    changes

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -9,6 +9,27 @@
         cidr_blocks      = [var.cidrs[1]]
     }
 
+    ingress {
+        from_port        = var.ports[2]
+        to_port          = var.ports[2]
+        protocol         = var.protocol[1]
+        cidr_blocks      = [var.cidrs[1]]
+    }
+
+    ingress {
+        from_port        = var.ports[3]
+        to_port          = var.ports[3]
+        protocol         = var.protocol[1]
+        cidr_blocks      = [var.cidrs[1]]
+    }
+
+    ingress {
+        from_port        = var.ports[4]
+        to_port          = var.ports[4]
+        protocol         = var.protocol[1]
+        cidr_blocks      = [var.cidrs[1]]
+    }
+
     egress {
         from_port        = var.ports[0]
         to_port          = var.ports[0]
---
+++ terraform/sg_config.tf
@@ -0,0 +1,28 @@
+variable "config" {
+   default = {
+    "server1" = {
+       ports  = [
+        {
+          from = 3000
+          to = 3000
+          source="0.0.0.0/0"
+        },
+        {
+          from = 22
+          to = 22
+          source="::/0"
+        },
+         {
+          from = 80
+          to = 80
+          source="0.0.0.0/0"
+        },
+        {
+          from = 443
+          to = 443
+          source ="0.0.0.0/0"
+        }
+      ]
+    }
+   } 
+ }
