21b84e719c55a0aff61861cdb0e4200214fd60c7
amuhtarov <amuhtarov@akumoproject.com>
2023-07-12 17:47:16 -0500

    changes

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -7,15 +7,17 @@ resource "aws_acm_certificate" "app" {
   }
 }
 
-resource "aws_acm_certificate_validation" "app" {
-  certificate_arn         = aws_acm_certificate.app.arn
-  validation_record_fqdns = [aws_route53_record.app_validation.fqdn]
-}
-
 resource "aws_route53_record" "app_validation" {
-  zone_id = "Z021882324FAK648HPE4P"
+  zone_id = "<your_route53_zone_id>"
   name    = "_acme-challenge.virmzi.de"
   type    = "CNAME"
   ttl     = "300"
   records = [aws_acm_certificate_validation.app.validation_record_fqdn]
+
+  depends_on = [aws_acm_certificate_validation.app]
+}
+
+resource "aws_acm_certificate_validation" "app" {
+  certificate_arn         = aws_acm_certificate.app.arn
+  validation_record_fqdns = [aws_route53_record.app_validation.fqdn]
 }
--- terraform/alb.tf
+++ terraform/alb.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "app" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.main_sg.id]
   subnets            = ["subnet-098ab2c379f487d8e", "subnet-00c67e8302ea518f9", "subnet-01b15e0262af666eb", "subnet-0b0a23f9960c0f286", "subnet-0995c7e89a01454d4", "subnet-0eebce24507f2bbb6"]
-  
+
   ip_address_type = "ipv4"
   enable_deletion_protection = false
 
@@ -27,5 +27,5 @@ resource "aws_lb_listener" "app" {
     target_group_arn = aws_lb_target_group.app.arn
   }
   ssl_policy      = "ELBSecurityPolicy-2016-08"  # Specify the SSL policy to use
-  certificate_arn = "<your_certificate_arn>"    # Replace with the ARN of your SSL certificate
+  certificate_arn = aws_acm_certificate.app.arn   # Use the ARN of the ACM certificate
 }
