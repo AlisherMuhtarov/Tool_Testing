59feec25d6956b97804acf841b99b1a77f6ac805
amuhtarov <amuhtarov@akumoproject.com>
2023-07-12 17:08:05 -0500

    changes

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -17,3 +17,13 @@ resource "aws_lb" "app" {
   }
 }
 
+resource "aws_lb_listener" "app" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
--- terraform/tg.tf
+++ terraform/tg.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "app" {
   name     = "elb-tg"
-  port     = 80
+  port     = 3000
   protocol = "HTTP"
   vpc_id   = "vpc-0c5d61951cc331de6"
 
