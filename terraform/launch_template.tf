4dbc6c0b52a4aeefd149e688d63112fbd9be1859
amuhtarov <amuhtarov@akumoproject.com>
2023-07-12 15:21:44 -0500

    Cahgnes

--- terraform/launch_template.tf
+++ terraform/launch_template.tf
@@ -25,13 +25,13 @@ resource "aws_launch_template" "app_asg_lc" {
   network_interfaces {
     associate_public_ip_address = true
     security_groups = [ aws_security_group.main_sg.id ]
+    vpc_security_group_ids = [ aws_security_group.main_sg.id ]
   }
 
   placement {
     availability_zone = "us-east-1a"
   }
 
-  vpc_security_group_ids = [ aws_security_group.main_sg.id ]
   tag_specifications {
     resource_type = "instance"
 
