b5207a34c7efd5c47a7704ebf60b03a8159c89bb
amuhtarov <amuhtarov@akumoproject.com>
2023-07-12 16:41:26 -0500

    changes

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -12,5 +12,5 @@ resource "aws_autoscaling_group" "app" {
 
 resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.app.name
-  alb_target_group_arn   = aws_lb_target_group.app.arn
+  lb_target_group_arn   = aws_lb_target_group.app.arn
 }
