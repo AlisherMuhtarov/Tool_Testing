d94299f007470ab6fe6c207eadc2f5d9b59fe211
amuhtarov <amuhtarov@akumoproject.com>
2023-07-13 14:13:20 -0500

    changes

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,85 +1,63 @@
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2_sg"
   description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = var.ports[5]
-    to_port     = var.ports[5]
-    protocol    = var.protocol[1]
-    security_groups = [aws_security_group.lb_sg.id]
-    cidr_blocks = [var.cidrs[1]]
-  }
-
-  ingress {
-    from_port   = var.ports[5]
-    to_port     = var.ports[5]
-    protocol    = var.protocol[1]
-    cidr_blocks = [var.cidrs[1]]
-  }
-
-  ingress {
-    from_port   = var.ports[3]
-    to_port     = var.ports[3]
-    protocol    = var.protocol[1]
-    cidr_blocks = [var.cidrs[0]]
-  }
-
-  ingress {
-    from_port   = var.ports[4]
-    to_port     = var.ports[4]
-    security_groups = [aws_security_group.lb_sg.id]
-    protocol    = var.protocol[1]
-    cidr_blocks = [var.cidrs[1]]
-  }
-
-  egress {
-    from_port   = var.ports[0]
-    to_port     = var.ports[0]
-    protocol    = var.protocol[0]
-    cidr_blocks = [var.cidrs[0]]
-  }
-
 }
 
 resource "aws_security_group" "lb_sg" {
   name        = "lb_sg"
   description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = var.ports[5]
-    to_port     = var.ports[5]
-    security_groups = [aws_security_group.ec2_sg.id]
-    protocol    = var.protocol[1]
-    cidr_blocks = [var.cidrs[1]]
-  }
-  ingress {
-    from_port   = var.ports[4]
-    to_port     = var.ports[4]
-    protocol    = var.protocol[1]
-    cidr_blocks = [var.cidrs[0]]
-  }
-
-  ingress {
-    from_port   = var.ports[3]
-    to_port     = var.ports[3]
-    security_groups = [aws_security_group.ec2_sg.id]
-    protocol    = var.protocol[1]
-    cidr_blocks = [var.cidrs[0]]
-  }
-
-  ingress {
-    from_port   = var.ports[3]
-    to_port     = var.ports[3]
-    protocol    = var.protocol[1]
-    cidr_blocks = [var.cidrs[0]]
-  }
-
-  egress {
-    from_port   = var.ports[0]
-    to_port     = var.ports[0]
-    protocol    = var.protocol[0]
-    cidr_blocks = [var.cidrs[0]]
-  }
-
 }
 
+resource "aws_security_group_rule" "ec2_to_lb" {
+  security_group_id = aws_security_group.ec2_sg.id
+  type              = "ingress"
+  from_port         = var.ports[5]
+  to_port           = var.ports[5]
+  protocol          = var.protocol[1]
+  source_security_group_id = aws_security_group.lb_sg.id
+}
+
+resource "aws_security_group_rule" "lb_to_ec2" {
+  security_group_id = aws_security_group.lb_sg.id
+  type              = "ingress"
+  from_port         = var.ports[5]
+  to_port           = var.ports[5]
+  protocol          = var.protocol[1]
+  source_security_group_id = aws_security_group.ec2_sg.id
+}
+
+resource "aws_security_group_rule" "allow_ssh_ec2" {
+  security_group_id = aws_security_group.ec2_sg.id
+  type              = "ingress"
+  from_port         = var.ports[3]
+  to_port           = var.ports[3]
+  protocol          = var.protocol[1]
+  cidr_blocks       = [var.cidrs[0]]
+}
+
+resource "aws_security_group_rule" "allow_http_lb" {
+  security_group_id = aws_security_group.lb_sg.id
+  type              = "ingress"
+  from_port         = var.ports[4]
+  to_port           = var.ports[4]
+  protocol          = var.protocol[1]
+  cidr_blocks       = [var.cidrs[0]]
+}
+
+resource "aws_security_group_rule" "allow_https_lb" {
+  security_group_id = aws_security_group.lb_sg.id
+  type              = "ingress"
+  from_port         = var.ports[3]
+  to_port           = var.ports[3]
+  protocol          = var.protocol[1]
+  cidr_blocks       = [var.cidrs[0]]
+}
+
+resource "aws_security_group_rule" "allow_outbound" {
+  security_group_id = aws_security_group.ec2_sg.id
+  type              = "egress"
+  from_port         = var.ports[0]
+  to_port           = var.ports[0]
+  protocol          = var.protocol[0]
+  cidr_blocks       = [var.cidrs[0]]
+}
