c3532e2a5a310c2e767e5d44fb2c681361eba2ff
amuhtarov <amuhtarov@akumoproject.com>
2023-07-13 11:27:34 -0500

    changes

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,17 +1,10 @@
-resource "aws_security_group" "main_sg" {
+resource "aws_security_group" "ec2_sg" {
   name        = "main"
   description = "Allow SSH inbound traffic"
 
   ingress {
     from_port   = var.ports[5]
     to_port     = var.ports[5]
-    protocol    = var.protocol[1]
-    cidr_blocks = [var.cidrs[1]]
-  }
-
-  ingress {
-    from_port   = var.ports[2]
-    to_port     = var.ports[2]
     protocol    = var.protocol[1]
     cidr_blocks = [var.cidrs[1]]
   }
@@ -38,3 +31,25 @@ resource "aws_security_group" "main_sg" {
   }
 
 }
+
+resource "aws_security_group" "lb_sg" {
+  name        = "main"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = var.ports[4]
+    to_port     = var.ports[4]
+    protocol    = var.protocol[1]
+    cidr_blocks = [var.cidrs[1]]
+  }
+
+  egress {
+    from_port   = var.ports[0]
+    to_port     = var.ports[0]
+    protocol    = var.protocol[0]
+    security_groups = [aws_security_group.ec2_sg]
+    cidr_blocks = [var.cidrs[0]]
+  }
+
+}
+
