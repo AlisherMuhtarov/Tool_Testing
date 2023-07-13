52d96ed4e6fc7997c9323bed77c3bd012dfb78cf
amuhtarov <amuhtarov@akumoproject.com>
2023-07-13 11:14:51 -0500

    changes

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -6,28 +6,28 @@ resource "aws_security_group" "main_sg" {
     from_port   = var.ports[5]
     to_port     = var.ports[5]
     protocol    = var.protocol[1]
-    cidr_blocks = [var.cidrs[0]]
+    cidr_blocks = [var.cidrs[1]]
   }
 
   ingress {
     from_port   = var.ports[2]
     to_port     = var.ports[2]
     protocol    = var.protocol[1]
-    cidr_blocks = [var.cidrs[0]]
+    cidr_blocks = [var.cidrs[1]]
   }
 
   ingress {
     from_port   = var.ports[3]
     to_port     = var.ports[3]
     protocol    = var.protocol[1]
-    cidr_blocks = [var.cidrs[0]]
+    cidr_blocks = [var.cidrs[1]]
   }
 
   ingress {
     from_port   = var.ports[4]
     to_port     = var.ports[4]
     protocol    = var.protocol[1]
-    cidr_blocks = [var.cidrs[0]]
+    cidr_blocks = [var.cidrs[1]]
   }
 
   egress {
