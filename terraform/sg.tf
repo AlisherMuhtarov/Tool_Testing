232529ec168ec89771894626dda9142658fdb6ec
amuhtarov <amuhtarov@akumoproject.com>
2023-07-13 15:05:57 -0500

    Changes

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -58,6 +58,7 @@ resource "aws_security_group_rule" "ec2_to_alb" {
   to_port           = var.ports[5]
   protocol          = var.protocol[1]
   cidr_blocks       = [var.cidrs[1]]
+  source_security_group_id = aws_security_group.lb_sg.id
   security_group_id = aws_security_group.ec2_sg.id
 }
 
@@ -67,6 +68,7 @@ resource "aws_security_group_rule" "ec2_to_alb-2" {
   to_port           = var.ports[4]
   protocol          = var.protocol[1]
   cidr_blocks       = [var.cidrs[1]]
+  source_security_group_id = aws_security_group.lb_sg.id
   security_group_id = aws_security_group.ec2_sg.id
 }
 
@@ -74,6 +76,7 @@ resource "aws_security_group_rule" "alb_to_ec2" {
     type = "ingress"
     from_port   = var.ports[5]
     to_port     = var.ports[5]
+    source_security_group_id = aws_security_group.ec2_sg.id
     security_group_id = aws_security_group.lb_sg.id
     protocol    = var.protocol[1]
     cidr_blocks = [var.cidrs[1]]
@@ -83,6 +86,7 @@ resource "aws_security_group_rule" "alb_to_ec2-2" {
     type = "ingress"
     from_port   = var.ports[3]
     to_port     = var.ports[3]
+    source_security_group_id = aws_security_group.ec2_sg.id
     security_group_id = aws_security_group.lb_sg.id
     protocol    = var.protocol[1]
     cidr_blocks = [var.cidrs[0]]
