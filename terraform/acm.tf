c4ce686b75f0725ae17422048bfdd36e40555426
amuhtarov <amuhtarov@akumoproject.com>
2023-07-12 21:13:42 -0500

    changes

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -5,8 +5,10 @@ resource "aws_acm_certificate" "app" {
 
 resource "aws_route53_record" "alb_record" {
   zone_id = "Z021882324FAK648HPE4P"
-  name    = "www.virmzi.de"
-  type    = "CNAME"
+  name    = [aws_acm_certificate.app.domain_validation_options[0].resource_record_name]
+  type    = [aws_acm_certificate.app.domain_validation_options[0].resource_record_type]
+  records = [aws_acm_certificate.app.domain_validation_options[0].resource_record_value]
+  ttl = 300
   alias {
     name                   = aws_lb.app.dns_name
     zone_id                = aws_lb.app.zone_id
