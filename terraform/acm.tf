64a74b45ad90484392d0e3e5d52aaab8734bba4a
amuhtarov <amuhtarov@akumoproject.com>
2023-07-13 10:36:20 -0500

    changes

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,27 +1,37 @@
 resource "aws_acm_certificate" "app" {
   domain_name       = "virmzi.de"
   validation_method = "DNS"
+
+  
+  validation_option {
+    domain_name       = "www.virmzi.de"
+    validation_domain = "virmzi.de"
+  }
 }
 
 
 resource "aws_route53_record" "app" {
-  for_each = {
-    for dvo in aws_acm_certificate.app.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+  name    = "${aws_acm_certificate.app.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.app.domain_validation_options.0.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.app.zone_id}"
+  records = ["${aws_acm_certificate.app.domain_validation_options.0.resource_record_value}"]
+  ttl     = "60"
+}
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.app.zone_id
+resource "aws_route53_record" "alb_record" {
+  zone_id = "Z021882324FAK648HPE4P"
+  name    = "www.virmzi.de"
+  type    = "A"
+  alias {
+    name                   = aws_lb.app.dns_name
+    zone_id                = aws_lb.app.zone_id
+    evaluate_target_health = true
+  }
 }
 
 resource "aws_acm_certificate_validation" "app" {
-  certificate_arn         = aws_acm_certificate.app.arn
-  validation_record_fqdns = [for record in aws_route53_record.app : record.fqdn]
+  certificate_arn = "${aws_acm_certificate.app.arn}"
+  validation_record_fqdns = [
+    "${aws_route53_record.app.fqdn}",
+  ]
 }
